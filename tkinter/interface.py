ab1b554646fccb6455c9507bca1dd9a2f35724fd
farhanmf30 <faarhaan.mf@gmail.com>
2019-05-13 14:39:13 +0700

    Update interface.py

--- tkinter/interface.py
+++ tkinter/interface.py
@@ -1,4 +1,17 @@
-import Tkinter
-top = Tkinter.Tk()
+#import Tkinter
+#top = Tkinter.Tk()
 # Code to add widgets will go here...
-top.mainloop()
+#top.mainloop()
+
+
+from tkinter import *
+window = Tk()
+window.title("capsa")
+
+
+window.geometry('500x500')
+
+btn = Button(window, text="login")
+btn.grid(column=1, row=0)
+
+window.mainloop()
