bf136feeddd266d9e0855886707613550f1475d6
Gani <ganiwijaya@live.com>
2019-05-19 22:17:01 +0700

    Update interface2.0.py

--- tkinter/interface2.0.py
+++ tkinter/interface2.0.py
@@ -17,35 +17,56 @@ class Window(Frame):
         file.add_command(label="Exit", command=self.client_exit)
         menu.add_cascade(label="File", menu=file)
         account = Menu(menu)
-        account.add_command(label="Login", command=self.client_exit)
+        account.add_command(label="Login", command=self.new_window)
         menu.add_cascade(label="Account", menu=account)
         self.frame = Frame(self.master)
-        self.b2 = Button(self.master, text="Button2", command=self.new_window)
+        self.b2 = Button(self.master, text="Login", command=self.new_window)
         self.b2.pack()
         self.frame.pack()
 
     def new_window(self):
         self.master.withdraw()
         self.newWindow = Toplevel(self.master)
-        bb = Buttons1(self.newWindow)
+        bb = Play(self.newWindow)
+
 
     def client_exit(self):
         exit()
 
-class Buttons1():
+class Play(Frame):
     
-    def __init__(self, master):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)             
         self.master = master
+        self.Login()
+
+    def Login(self):
+        self.master.title("GUI")
+        self.pack(fill=BOTH, expand=1)
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+        file = Menu(menu)
+        file.add_command(label="Exit", command=self.client_exit)
+        menu.add_cascade(label="File", menu=file)
+        account = Menu(menu)
+        account.add_command(label="Login", command=self.new_window)
+        menu.add_cascade(label="Account", menu=account)
         self.frame = Frame(self.master)
-        self.b3 = Button(self.master, text="Button3", command=self.display3)
-        self.b3.pack()
+        self.b2 = Button(self.master, text="Login", command=self.new_window)
+        self.b2.pack()
         self.frame.pack()
 
-    def display3(self):
-        print 'hello button3'
+    def new_window(self):
+        self.master.withdraw()
+        self.newWindow = Toplevel(self.master)
+        bb = Play(self.newWindow)
+
+
+    def client_exit(self):
+        exit()
 
 
 root = Tk()
-root.geometry("400x300")
+root.geometry("200x100")
 app = Window(root)
 root.mainloop()  
