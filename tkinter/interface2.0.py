41c7de127592bb4410d12442f527b0805071149a
Gani <ganiwijaya@live.com>
2019-05-19 10:38:29 +0700

    Update interface2.0.py

--- tkinter/interface2.0.py
+++ tkinter/interface2.0.py
@@ -1,3 +1,4 @@
+import Tkinter
 from Tkinter import Tk, Label, Button
 
 class MyFirstGUI:
@@ -11,12 +12,16 @@ class MyFirstGUI:
         self.greet_button = Button(master, text="Login", command=self.login)
         self.greet_button.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button = Button(master, text="Close", command=self.client_exit)
         self.close_button.pack()
 
     def login(self):
         print("Greetings!")
 
+    def client_exit(self):
+        exit()
+
 root = Tk()
 my_gui = MyFirstGUI(root)
+root.geometry("400x300")
 root.mainloop()
