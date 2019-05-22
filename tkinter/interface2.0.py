1d54d5c97d727fc972b73d76301625f8169e5c1a
farhanmf30 <faarhaan.mf@gmail.com>
2019-05-22 21:33:20 +0700

    Update interface2.0.py
    update emai

--- tkinter/interface2.0.py
+++ tkinter/interface2.0.py
@@ -82,6 +82,31 @@ class Play(Frame):
         exit()
 
 
+class email(frame):
+    def test_function(entry):
+    	print("input email:", entry)
+
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+frame = tk.Frame(root, bg='#80c1ff', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry = tk.Entry(frame, font=40)
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text="play", font=40, command=lambda: play(entry.get()))
+button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
+
 root = Tk()
 root.geometry("800x600")
 app = Window(root)
