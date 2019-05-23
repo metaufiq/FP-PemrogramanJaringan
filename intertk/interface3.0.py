ed6038d178fee05c7f0f5543bb29db2e2f11179a
Gani <ganiwijaya@live.com>
2019-05-23 13:01:34 +0700

    Update interface3.0.py

--- intertk/interface3.0.py
+++ intertk/interface3.0.py
@@ -1,5 +1,6 @@
 import Tkinter as tk
 from PIL import Image, ImageTk
+import tkMessageBox
 
 HEIGHT = 700
 WIDTH = 800
@@ -16,16 +17,21 @@ load = Image.open("./asset logo/instagram_profile_image.png").resize((400, 400),
 render = ImageTk.PhotoImage(load)
 img = tk.Label(frame, image=render, bg='#fff3e1')
 img.image = render
-img.place(relx=0.35, rely=0.2, relwidth=0.3, relheight=0.3)
+img.place(relx=0.35, rely=0.16, relwidth=0.3, relheight=0.3)
 
 label = tk.Label(frame, text="Masukkan Email Anda", bg='#fff3e1')
 label.place(relx=0.37, rely=0.55, relwidth=0.26)
 
-entry = tk.Entry(frame, bg='white')
+entry = tk.Entry(frame, bg='white', bd=0)
 entry.place(relx=0.37, rely=0.6,relwidth=0.26, relheight=0.05)
 
-
-button = tk.Button(frame, text="Mulai", bg='black', fg='white', font=40)
+button = tk.Button(frame, text="Mulai", bg='black', fg='white', font=40, bd=0)
 button.place(relx=0.44, rely=0.67, relwidth=0.12, relheight=0.05)
+
+def tentang():
+   tkMessageBox.showinfo( "Tentang", "Capsa oleh kelompok 2")
+
+button = tk.Button(frame, text="Tentang", bg='#fff3e1', fg='black', bd=0, command= tentang)
+button.place(relx=0.44, rely=0.8, relwidth=0.12, relheight=0.05)
 
 root.mainloop()
