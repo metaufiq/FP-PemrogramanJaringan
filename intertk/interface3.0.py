bccb65295eb6f023b78134e8c2411afb0bb32ca0
Gani <ganiwijaya@live.com>
2019-05-23 12:50:32 +0700

    Update interface3.0.py

--- intertk/interface3.0.py
+++ intertk/interface3.0.py
@@ -12,14 +12,14 @@ canvas.pack()
 frame = tk.Frame(root, bg='#fff3e1')
 frame.place(relx=0, rely=0, relwidth=1, relheight=1)
 
-load = Image.open("./asset logo/instagram_profile_image.png").resize((300, 300), Image.ANTIALIAS)
+load = Image.open("./asset logo/instagram_profile_image.png").resize((400, 400), Image.ANTIALIAS)
 render = ImageTk.PhotoImage(load)
 img = tk.Label(frame, image=render, bg='#fff3e1')
 img.image = render
 img.place(relx=0.35, rely=0.2, relwidth=0.3, relheight=0.3)
 
 label = tk.Label(frame, text="Masukkan Email Anda", bg='#fff3e1')
-label.place(relx=0.37, rely=0.55)
+label.place(relx=0.37, rely=0.55, relwidth=0.26)
 
 entry = tk.Entry(frame, bg='white')
 entry.place(relx=0.37, rely=0.6,relwidth=0.26, relheight=0.05)
