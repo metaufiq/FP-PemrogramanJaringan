39770dd4d4e807036034d8c875c11b4a56d76092
Gani <ganiwijaya@live.com>
2019-05-23 22:15:14 +0700

    update

--- intertk/gameplay.py
+++ intertk/gameplay.py
@@ -26,17 +26,19 @@ class GamePlay():
 		canvaschat = tk.Canvas(window, height=HEIGHT, width=WIDTH)
 		canvaschat.pack()
 
-		framechat = tk.Frame(window, bg='#fff3e1')
-		framechat.place(relx=0, rely=0, relwidth=1, relheight=1)
+		self.framechat = tk.Frame(window, bg='#fff3e1')
+		self.framechat.place(relx=0, rely=0, relwidth=1, relheight=1)
 
-		entrychat = tk.Entry(framechat, bg='white', bd=0, justify='left')
+		entrychat = tk.Entry(self.framechat, bg='white', bd=0, justify='left')
 		entrychat.place(relx=0.05, rely=0.8,relwidth=0.7, relheight=0.12)
 
-		button = tk.Button(framechat, text="Kirim", bg='black', fg='white', bd=0, activebackground='#fff3e1', command=lambda: self.printchat(entrychat.get()))
+		button = tk.Button(self.framechat, text="Kirim", bg='black', fg='white', bd=0, activebackground='#fff3e1', command=lambda: self.printchat(entrychat.get()))
 		button.place(relx=0.75, rely=0.8, relwidth=0.2, relheight=0.12)
 
 	def printchat(self,entrychat):
 		print entrychat
+		labelchat = tk.Label(self.framechat, text=entrychat, bg='#fff3e1', font=50)
+		labelchat.place(relx=0.37, rely=0.55, relwidth=0.26)
 
 	def playGame(self,root):
 		frameGame = tk.Frame(root, bg='#000000')
